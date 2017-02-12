71b673caff99e9105cca83dc67f486dc16190d9e
Fuller <fullerar@l14365.cs.jmu.edu>
2017-02-12 16:37:57 -0500

    updated create.sql with some more tables/added keys

--- schema/create.sql
+++ schema/create.sql
@@ -1,8 +1,14 @@
 DROP TABLE IF EXISTS division;
 
 CREATE TABLE division (
-    div_num integer NOT NULL,
-    div_name text NOT NULL
+    div_num integer NOT NULL PRIMARY KEY,
+    div_name text NOT NULL,
+    loc_code integer NOT NULL,
+    loc_type text NOT NULL,
+	region integer NOT NULL,
+	city text NOT NULL,
+	state text NOT NULL,
+	zip text NOT NULL 
 );
 
 ALTER TABLE division OWNER TO nectar;
@@ -10,17 +16,27 @@ ALTER TABLE division OWNER TO nectar;
 COMMENT ON TABLE division IS 'all divisions in Virginia';
 
 
+
+
+
 DROP TABLE IF EXISTS school;
 
 CREATE TABLE school (
     div_num integer NOT NULL,
     sch_num integer NOT NULL,
-    sch_name text NOT NULL
+    sch_name text NOT NULL,
+	city text NOT NULL,
+	state text NOT NULL,
+	zip text NOT NULL,
+	PRIMARY KEY(div_num, sch_num)
 );
 
 ALTER TABLE school OWNER TO nectar;
 
 COMMENT ON TABLE school IS 'all schools in Virginia';
+
+
+
 
 
 DROP TABLE IF EXISTS enroll;
@@ -35,12 +51,72 @@ CREATE TABLE enroll (
     lep text NOT NULL,
     disadva text NOT NULL,
     grade_num integer NOT NULL,
-    fall_cnt integer NOT NULL
+    fall_cnt integer NOT NULL,
+	PRIMARY KEY(div_num, sch_num, sch_year)
 );
 
 ALTER TABLE enroll OWNER TO nectar;
 
 COMMENT ON TABLE enroll IS 'fall membership counts';
+
+
+
+
+
+DROP TABLE IF EXIST sol_data;
+
+CREATE TABLE sol_data (
+	sch_year integer NOT NULL,
+	div_num integer NOT NULL,
+	sch_num integer NOT NULL,	
+    race text NOT NULL,
+    gender text NOT NULL,
+    disabil text NOT NULL,
+    lep text NOT NULL,
+    disadva text NOT NULL,
+	subject text NOT NULL,
+	test_name text NOT NULL,
+	test_level text NOT NULL,
+	avg_score integer,
+	pass_adv real NOT NULL,
+	pass_prof real NOT NULL,
+	pass_rate real NOT NULL,
+	fail_rate real NOT NULL,
+	PRIMARY KEY (sch_year, div_num, sch_num)
+);
+
+ALTER TABLE sol_data OWNER TO nectar;
+
+COMMENT ON TABLE sol_data IS 'sol test data per division/school';
+
+
+
+DROP TABLE IF EXISTS postsec_enroll
+
+CREATE TABLE postsec_enroll(
+	sch_year integer NOT NULL,
+	div_num integer NOT NULL,
+	sch_num integer NOT NULL,
+    race text NOT NULL,
+    gender text NOT NULL,
+    disabil text NOT NULL,
+    lep text NOT NULL,
+    disadva text NOT NULL,
+	enroll_graduate_cnt integer NOT NULL,
+	ps_institution_type integer NOT NULL,
+	ps_enrollement_cnt integer NOT NULL,
+	PRIMARY KEY(div_num, sch_num, sch_year)
+);
+
+ALTER TABLE postsec_enroll OWNER TO nectar;
+
+COMMENT ON TABLE postsec_enroll IS 'post secondary enrollment data per division/school';
+
+
+
+
+
+
 
 
 DROP TABLE IF EXISTS college;
