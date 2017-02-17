d5984884689e14cd5d8f79604d90824b838fcd7d
Brandon Bautista <bautisbm@dukes.jmu.edu>
2017-02-16 20:29:12 -0500

    fixed a syntax error in copy.sh

--- schema/copy.sh
+++ schema/copy.sh
@@ -81,5 +81,5 @@ echo COPY college FROM csv
 psql -c "\copy college FROM college.csv WITH CSV HEADER" nectar
 
 
-echo COPY cte_comp  FROM csv
+echo COPY cte_comp FROM csv
 psql -c "\copy cte_comp FROM cte_comp.csv WITH CSV HEADER" nectar
