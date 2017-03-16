3f0f0651777b79ff1e3bf672db1dc92bb0073192
Fuller <fullerar@l14365.cs.jmu.edu>
2017-03-16 19:19:16 -0400

    added two functions to database, worked on norm function

--- queries/highSchs_per_stemDiv.sql
+++ queries/highSchs_per_stemDiv.sql
@@ -3,21 +3,21 @@
 --
 
 
---DROP FUNCTION IF EXISTS highSchs_per_stemDiv(div_name text);
+DROP FUNCTION IF EXISTS highSchs_per_stemDiv(div_name text);
 
---CREATE FUNCTION highSchs_per_stemDiv(div_name text)
---RETURNS TABLE(sch_num integer, sch_name text) AS $$
+CREATE FUNCTION highSchs_per_stemDiv(div_name text)
+RETURNS TABLE(sch_num integer, sch_name text) AS $$
 
 SELECT DISTINCT sch.sch_num, sch.sch_name
 FROM stem_schools AS stem
 	JOIN school AS sch ON stem.div_num = sch.div_num
-WHERE stem.div_num = $1
+WHERE stem.div_name = $1
 	AND sch.sch_num NOT IN(8888,9997)
 	AND sch.sch_name LIKE '%High%'
 
---$$ LANGUAGE SQL STABLE STRICT;
+$$ LANGUAGE SQL STABLE STRICT;
 
---ALTER FUNCTION highSchs_per_stemDiv(div_name text) OWNER TO nectar;
+ALTER FUNCTION highSchs_per_stemDiv(div_name text) OWNER TO nectar;
 
 
 
--- queries/stemDiv_solSubResultAvgs.sql
+++ queries/stemDiv_solSubResultAvgs.sql
@@ -3,40 +3,29 @@
 --
 
 
---DROP FUNCTION IF EXISTS stemDiv_solSubResultAvgs(div_name text, subject text);
+DROP FUNCTION IF EXISTS stemDiv_solSubResultAvgs(div_name text, subject text);
 
---CREATE FUNCTION stemDiv_solSubResultAvgs(div_name text, subject text)
---RETURNS TABLE(sch_year integer, div_name text, avg_score float) AS $$
+CREATE FUNCTION stemDiv_solSubResultAvgs(div_name text, subject text)
+RETURNS TABLE(sch_year integer, div_name text, avg_score decimal(3,2)) AS $$
 
-SELECT DISTINCT sch_year, stem.div_num AS div, AVG(avg_score) AS avg1 
+SELECT DISTINCT sch_year, stem.div_name AS div, CAST(AVG(avg_score) AS DECIMAL(10,2))
 FROM stem_schools AS stem
-	JOIN school AS sch ON stem.div_num = sch.div_num
-	JOIN sol_test_data AS sol ON sch.div_num = sol.div_num AND sch.sch_num = sol.sch_num
+        JOIN school AS sch ON stem.div_num = sch.div_num
+        JOIN sol_test_data AS sol ON sch.div_num = sol.div_num AND sch.sch_num = sol.sch_num
 WHERE div_name = $1
-	AND sch.sch_name LIKE '%High%'
-	AND sol.subject = $2
-	AND sol.test_name = 'ALL'
-	AND race = 'ALL'
-	AND gender = 'ALL'
-	AND disabil = 'ALL'
-	AND lep = 'ALL'
-	AND disadva = 'ALL'
-	AND sch_year >= 2012
+        AND sch.sch_name LIKE '%High%'
+        AND sol.subject = $2
+        AND sol.test_name = 'ALL'
+        AND race = 'ALL'
+        AND gender = 'ALL'
+        AND disabil = 'ALL'
+        AND lep = 'ALL'
+        AND disadva = 'ALL'
+        AND sch_year >= 2012
 GROUP BY sch_year, div
 ORDER BY sch_year, div
 
 
---$$ LANGUAGE SQL STABLE STRICT;
+$$ LANGUAGE SQL STABLE STRICT;
 
---ALTER FUNCTION stemDiv_solSubResultAvgs(div_name text, subject text) OWNER TO nectar;
-
-
-
-
-
-
-
-
-
-
-
+ALTER FUNCTION stemDiv_solSubResultAvgs(div_name text, subject text) OWNER TO nectar;
