1329476cf7d9acbc29167d15fd7f461196e3d969
Fuller <fullerar@l14365.cs.jmu.edu>
2017-03-19 14:56:18 -0400

    reworked current sql functions

---
+++ queries/highSchScores_per_StemDiv.sql
@@ -0,0 +1,30 @@
+﻿
+-- Returns the avg subject sol scores of each high school in a division
+--
+
+DROP FUNCTION IF EXISTS highSchScores_per_StemDiv(div_name text, subject text);
+
+CREATE FUNCTION highSchScores_per_StemDiv(div_name text, subject text)
+RETURNS TABLE(sch_year integer, sch_num integer, sch_name text, avg_score integer) AS $$
+
+
+
+SELECT DISTINCT sol.sch_year, sch.sch_num, sch.sch_name, avg_score
+FROM stem_schools AS stem
+	JOIN school AS sch ON stem.div_num = sch.div_num
+	JOIN sol_test_data AS sol ON sch.div_num = sol.div_num AND sch.sch_num = sol.sch_num
+WHERE stem.div_name = $1
+	AND sch.sch_num NOT IN(8888,9997)
+	AND sch.sch_name LIKE '%High%'
+	AND sol.subject = $2
+	AND sol.test_name = 'ALL'
+	AND race = 'ALL'
+	AND gender = 'ALL'
+	AND disabil = 'ALL'
+	AND lep = 'ALL'
+	AND disadva = 'ALL'
+ORDER BY sch_year, sch_num
+
+$$ LANGUAGE SQL STABLE STRICT;
+
+ALTER FUNCTION highSchScores_per_StemDiv(div_name text, subject text) OWNER TO nectar;
---
+++ schema/indexes.sql
@@ -0,0 +1,15 @@
+CREATE INDEX ON division (div_num);
+CREATE INDEX ON division (div_num, div_name);
+
+CREATE INDEX ON school (div_num, sch_num);
+
+CREATE INDEX ON sol_test_data (sch_year);
+CREATE INDEX ON sol_test_data (div_num, sch_num);
+
+CREATE INDEX ON ontime_cohort (sch_year);
+CREATE INDEX ON ontime_cohort (div_num, sch_num);
+
+CREATE INDEX ON postsec_enroll (sch_year);
+CREATE INDEX ON postsec_enroll (div_num, sch_num);
+
+CREATE INDEX ON stem_schools (div_num, sch_num);
