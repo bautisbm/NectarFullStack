4b412a3eb9cec731dd4e7a5066d428a0ff16a202
Brandon Bautista <bautisbm@dukes.jmu.edu>
2017-03-31 10:27:33 -0400

    changed stemDiv again again again

--- queries/stemDiv_solSubResultAvgs.sql
+++ queries/stemDiv_solSubResultAvgs.sql
@@ -3,12 +3,12 @@
 --
 
 
-DROP FUNCTION IF EXISTS stemdiv_solsubresultavgs(div_num integer, subject text, race text, gender text, disabil text, lep text, disadva text);
+DROP FUNCTION IF EXISTS stemDiv_SolSubResultavgs(div_num integer, subject text, race text, gender text, disabil text, lep text, disadva text);
 
-CREATE FUNCTION stemdiv_solsubresultavgs(div_num integer, subject text, race text, gender text, disabil text, lep text, disadva text)
+CREATE FUNCTION stemDiv_SolSubResultavgs(div_num integer, subject text, race text, gender text, disabil text, lep text, disadva text)
 RETURNS TABLE(sch_year integer, div_name text, avg_score numeric) AS $$
 
-SELECT DISTINCT sch_year, div_name, CAST(AVG(avg_score) AS DECIMAL(10,2))
+SELECT DISTINCT sch_year, div_name AS div, CAST(AVG(avg_score) AS DECIMAL(10,2))
 FROM stem_schools AS stem
         JOIN school AS sch ON stem.div_num = sch.div_num
         JOIN sol_test_data AS sol ON sch.div_num = sol.div_num AND sch.sch_num = sol.sch_num
@@ -28,4 +28,4 @@ ORDER BY sch_year
 
 $$ LANGUAGE SQL STABLE STRICT;
 
-ALTER FUNCTION stemdiv_solsubresultavgs(div_num integer, subject text, race text, gender text, disabil text, lep text, disadva text) OWNER TO nectar;
+ALTER FUNCTION stemDiv_SolSubResultavgs(div_num integer, subject text, race text, gender text, disabil text, lep text, disadva text) OWNER TO nectar;
