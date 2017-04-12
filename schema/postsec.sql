a96168ea6bf162a2bcb9cbb0d18b12f4e1d0e51f
Fuller <fullerar@student.cs.jmu.edu>
2017-04-12 12:53:25 -0400

    postsec

--- schema/postsec.sql
+++ schema/postsec.sql
@@ -6,17 +6,17 @@
 INSERT INTO spelling VALUES ('092', '0930', 'Pocahontas High/Middle');
 
 -- verify replacement table (510 unique: div_num, sch_num, sch_name)
-SELECT DISTINCT p.div_num, p.sch_num, p.sch_name, s.sch_num AS fix_num
+SELECT DISTINCT p.div_num, p.sch_num, s.sch_num AS fix_num
 FROM postsec_enroll AS p LEFT JOIN spelling AS s
      ON p.div_num::text = s.div_num
     AND (p.sch_num IS NULL AND s.sch_num IS NULL
         OR (s.sch_num LIKE p.sch_num::text || '_')) -- missing digit
-    AND p.sch_name = s.sch_name
-ORDER BY p.div_num, p.sch_num, p.sch_name, s.sch_num;
+    --AND p.sch_name = s.sch_name
+ORDER BY p.div_num, p.sch_num, s.sch_num;
 
 CREATE TABLE postsec_enroll2 AS
   SELECT
-    p.school_year,
+    p.sch_year,
     p.div_num, 
     s.sch_num AS sch_num,
     federal_race_code AS race, 
@@ -30,8 +30,8 @@ CREATE TABLE postsec_enroll2 AS
   FROM postsec_enroll AS p, spelling AS s
   WHERE p.div_num = s.div_num
     AND (p.sch_num IS NULL AND s.sch_num IS NULL
-        OR (s.sch_num LIKE p.sch_num || '_')) -- missing digit
-    AND p.sch_name = s.sch_name
+        OR (s.sch_num LIKE p.sch_num || '_')); -- missing digit
+    --AND p.sch_name = s.sch_name
 
 DROP TABLE spelling;
 
